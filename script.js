8957dc40dd5abf2d1e7107a7013e64cee2a961fd
Matheus <105922777+Matheus-procopio@users.noreply.github.com>
2024-08-07 17:41:19 -0300

    kkkkkkkkkkkkkkkk

--- script.js
+++ script.js
@@ -96,11 +96,14 @@ function mostraPergunta() {
     mostraAlternativas();
 }
 function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas) {
-        const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
-        caixaAlternativas.appendChild(botaoAlternativas);
+    for(const alternativa of perguntaAtual.alternativas){
+            const botaoAlternativas = document.createElement("button");
+            botaoAlternativas.textContent = alternativa.texto;
+            botaoAlternativas.addEventListener("click", function(){
+                atual++;
+                mostraPergunta();
+            })
+            caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
 function respostaSelecionada(opcaoSelecionada) {
